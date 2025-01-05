562c593f35b741ef6c50b43a43a56ea7fd1165c8
ArayisZzz <arayzzz@vip.qq.com>
2025-01-05 17:19:41 +0800

    Update

--- ad.py
+++ ad.py
@@ -2,32 +2,32 @@ import re
 import requests
 import time
 
-rules_url = ['https://github.com/uBlockOrigin/uAssets/blob/master/filters/filters.txt',
+rules_url = ['filters.txt',
              #Ads
-             'https://github.com/uBlockOrigin/uAssets/blob/master/filters/badlists.txt',
+             'badlists.txt',
              #Badware risks
-             'https://github.com/uBlockOrigin/uAssets/blob/master/filters/privacy.txt',
+             'privacy.txt',
              #Privacy
-             'https://github.com/uBlockOrigin/uAssets/blob/master/filters/quick-fixes.txt',
+             'quick-fixes.txt',
              #Quick fixes
-             'https://github.com/uBlockOrigin/uAssets/blob/master/filters/unbreak.txt',
+             'unbreak.txt',
              #Unbreak
-             'https://easylist.to/easylist/easylist.txt',
+             'easylist.txt',
              #EasyList
-             'https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt',
-             'https://filters.adtidy.org/extension/ublock/filters/11.txt',
-             'https://curbengh.github.io/malware-filter/urlhaus-filter-ag-online.txt',
-             'https://filters.adtidy.org/extension/ublock/filters/224.txt',
+             '2_without_easylist.txt',
+             '11.txt',
+             'urlhaus-filter-ag-online.txt',
+             '224.txt',
              #Anti-AD
-             'https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-adguard.txt'
+             'anti-ad-adguard.txt'
              ]
 f2 = open("ad.conf","a",encoding='utf-8')
 f2.write("[General]\nipv6 = false\nbypass-system = true\nskip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, fe80::/10, fc00::/7, localhost, *.local, *.lan, *.internal, e.crashlytics.com, captive.apple.com, sequoia.apple.com, seed-sequoia.siri.apple.com, *.ls.apple.com\nbypass-tun = 10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.0.0.0/24,192.0.2.0/24,192.88.99.0/24,192.168.0.0/16,198.18.0.0/15,198.51.100.0/24,203.0.113.0/24,233.252.0.0/24,224.0.0.0/4,255.255.255.255/32,::1/128,::ffff:0:0/96,::ffff:0:0:0/96,64:ff9b::/96,64:ff9b:1::/48,100::/64,2001::/32,2001:20::/28,2001:db8::/32,2002::/16,3fff::/20,5f00::/16,fc00::/7,fe80::/10,ff00::/8\ndns-server = https://dns.adguard-dns.com/dns-query, https://security.cloudflare-dns.com/dns-query, https://doh.pub/dns-query\n")
 f2.write("[Rule]\n")
 
 for url in rules_url:
-    x = requests.get(url)
-    for str in x.content:
+    f1 = open(url,"r",encoding='utf-8')
+    for str in f1:
         flag = 0
         it = re.finditer('^\|\|[0-9.]+\^$',str)
         for match in it:
@@ -39,7 +39,8 @@ for url in rules_url:
         it = re.finditer('^\|\|[\w.-]+\^$',str)
         for match in it:
             f2.write("DOMAIN_SUFFIX,"+match.string[2:len(match.string)-2]+",REJECT\n")
-        time.sleep(5)
+        time.sleep(1)
+    f1.close
 #f1 = open("11.txt","r",encoding='utf-8')
 
 f2.write("GEOIP,PRIVATE,DIRECT\nGEOIP,FACEBOOK,PROXY\nGEOIP,CN,DIRECT\nFINAL,PROXY\n")
