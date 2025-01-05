dd9314618ed6bf8275618a4b6447a4d549d01ea9
ArayisZzz <arayzzz@vip.qq.com>
2025-01-05 16:33:29 +0800

    Update

---
+++ Pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+curl -O https://github.com/uBlockOrigin/uAssets/blob/master/filters/filters.txt
+#Ads
+curl -O https://github.com/uBlockOrigin/uAssets/blob/master/filters/badlists.txt
+#Badware risks
+curl -O https://github.com/uBlockOrigin/uAssets/blob/master/filters/privacy.txt
+#Privacy
+curl -O https://github.com/uBlockOrigin/uAssets/blob/master/filters/quick-fixes.txt
+#Quick fixes
+curl -O https://github.com/uBlockOrigin/uAssets/blob/master/filters/unbreak.txt
+#Unbreak
+curl -O https://easylist.to/easylist/easylist.txt
+#EasyList
+curl -O https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt
+curl -O https://filters.adtidy.org/extension/ublock/filters/11.txt
+curl -O https://curbengh.github.io/malware-filter/urlhaus-filter-ag-online.txt
+curl -O https://filters.adtidy.org/extension/ublock/filters/224.txt
+#Anti-AD
+curl -O https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-adguard.txt
