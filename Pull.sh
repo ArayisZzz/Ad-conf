02a56eaec81da999ade37b54296072d422ef9362
ArayisZzz <arayzzz@vip.qq.com>
2025-01-06 10:31:38 +0800

    Update

--- Pull.sh
+++ Pull.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/filters.txt
-#Ads
-curl -O https://ublockorigin.github.io/uAssetsCDN/filters/badware.min.txt
-#Badware risks
-curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/privacy.txt
-#Privacy
-curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/quick-fixes.txt
-#Quick fixes
-curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/unbreak.txt
-#Unbreak
-curl -O https://easylist.to/easylist/easylist.txt
-#EasyList
-curl -O https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt
-curl -O https://filters.adtidy.org/extension/ublock/filters/11.txt
-curl -O https://curbengh.github.io/malware-filter/urlhaus-filter-ag-online.txt
-curl -O https://filters.adtidy.org/extension/ublock/filters/224.txt
 #Anti-AD
 curl -O https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-adguard.txt
+cat /dev/null > ad.conf
+#curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/filters.txt
+#Ads
+#curl -O https://ublockorigin.github.io/uAssetsCDN/filters/badware.min.txt
+#Badware risks
+#curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/privacy.txt
+#Privacy
+#curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/quick-fixes.txt
+#Quick fixes
+#curl -O https://raw.githubusercontent.com/uBlockOrigin/uAssets/refs/heads/master/filters/unbreak.txt
+#Unbreak
+#curl -O https://easylist.to/easylist/easylist.txt
+#EasyList
+#curl -O https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt
+#curl -O https://filters.adtidy.org/extension/ublock/filters/11.txt
+#curl -O https://curbengh.github.io/malware-filter/urlhaus-filter-ag-online.txt
+#curl -O https://filters.adtidy.org/extension/ublock/filters/224.txt
